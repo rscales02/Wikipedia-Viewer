ecc1b030ec6bf86d353383c446440ac7f4b1c427
rscales02 <rscales02@yahoo.com>
2017-09-13 14:25:16 -0600

    many fixed issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,35 @@
-$('#search-btn').on('click', function (event) {
-	/* Act on the event */
-	var searchTerm = $('#search-term').val();
-	var url = 'https://en.wikipedia.org//w/api.php?action=opensearch&search=' + searchTerm + '&utf8=1';
-	console.log(searchTerm);
+$(document).ready(function() {
+	var data = "";
 
-	$.ajax({
-		url: 'url',
-		type: 'GET',
-		dataType: 'json',
-	})
-	.done(function() {
-		console.log("success");
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
-	});
-	
+    $('#search-btn').on('click', function(event) {
+        /* Act on the event */
+        var searchTerm = $('#search-term').val();
+        var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchTerm;
+
+        $.ajax({
+                type: 'GET',
+                url: url,
+                dataType: 'json',
+                data: {
+                	format: 'json',
+                	origin: '*'
+                },
+                success: function(response) {
+                	data = response;
+                	
+                }
+            })
+            .done(function() {
+
+                console.log("success");
+            })
+            .fail(function() {
+                console.log("error");
+            })
+            .always(function() {
+                console.log("complete");
+            });
+
+    });
+    console.log(data);
 });
