b38810f644a341f6323b7af53a077decd3915ec1
rscales02 <rscales02@yahoo.com>
2017-09-13 12:18:58 -0600

    jasjs

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
-$('#search-btn').on('click', function(event) {
+
+
+$('#search-btn').on('click', function (event) {
 	/* Act on the event */
 	var searchTerm = $('#search-term').val();
+	var url = 'https://en.wikipedia.org//w/api.php?action=opensearch&search=' + searchTerm + '&utf8=1';
 	console.log(searchTerm);
+
+	$.ajax({
+		url: 'url',
+		type: 'GET',
+		dataType: 'json',
+		data: {param1: 'value1'},
+	})
+	.done(function() {
+		console.log("success");
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+	
 });
