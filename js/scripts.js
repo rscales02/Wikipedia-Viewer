7e85a2ab06185e4be4f39b4e20d305b032c5fbef
rscales02 <rscales02@yahoo.com>
2017-09-14 14:36:09 +0200

    ajax works, now data handle broken

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,23 @@
 $(document).ready(function() {
-	var data = "";
-
     $('#search-btn').on('click', function(event) {
         /* Act on the event */
         var searchTerm = $('#search-term').val();
         var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchTerm;
+        var data = "";
 
         $.ajax({
                 type: 'GET',
                 url: url,
                 dataType: 'json',
                 data: {
-                	format: 'json',
-                	origin: '*'
+                    format: 'json',
+                    origin: '*'
                 },
                 success: function(response) {
-                	data = response;
-                	
+                    data = response;
                 }
             })
             .done(function() {
-
                 console.log("success");
             })
             .fail(function() {
@@ -29,7 +26,13 @@ $(document).ready(function() {
             .always(function() {
                 console.log("complete");
             });
-
+    	
+    	function dataHandler () {
+    		console.log(data);
+    		for (var i = 0; i < data[0].length; i++) {
+    			$('#output').html('<div class=\"container\"><a href=\"' + data[2][i]+ '\">' + data[0][i] '</a><p>' + data[1][i] + '</p></div>');
+    		}
+    	}
     });
-    console.log(data);
+
 });
