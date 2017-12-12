c0632f027dd675c146d43c1de75552d2eb58d100
rscales02 <rscales02@yahoo.com>
2017-12-12 12:13:21 -0500

    a

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,41 @@
-$(document).ready(function() {
-    $('#search-btn').on('click', function(event) {
+$(document).ready(function () {
+    $('#search-btn').on('click', function (event) {
         /* Act on the event */
         var searchTerm = $('#search-term').val();
         var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchTerm;
         var data = "";
 
         $.ajax({
-                type: 'GET',
-                url: url,
-                dataType: 'json',
-                data: {
-                    format: 'json',
-                    origin: '*'
-                },
-                success: function(response) {
-                    data = response;
-                    console.log(data);
-    				
-                }
-            })
-            .done(function() {
+            type: 'GET',
+            url: url,
+            dataType: 'json',
+            data: {
+                format: 'json',
+                origin: '*'
+            },
+            success: function (response) {
+                data = response;
+                console.log(data);
+                dataHandler(data);
+            }
+        })
+            .done(function () {
                 console.log("success");
             })
-            .fail(function() {
+            .fail(function () {
                 console.log("error");
             })
-            .always(function() {
+            .always(function () {
                 console.log("complete");
             });
-        function dataHandler() {
-            for (var i = 0; i < data[1].length; i++) {
-                    $('#output').append('<div class=\"container\"><a href=\"' + data[3][i] + '\">' + data[1][i] + '</a><p>' + data[2][i] + '</p></div>');
-            }
-        }
+
     });
 
 });
+
+function dataHandler(data) {
+    console.log('data handle')
+    for (var i = 0; i < data[1].length; i++) {
+        $('#output').append('<div class=\"container\"><a href=\"' + data[3][i] + '\">' + data[1][i] + '</a><p>' + data[2][i] + '</p></div>');
+    }
+}
